b97413ee45a1edd4d7bccf27c7c6d5dda51ba5ce
rileywallaceriley <67809682+rileywallaceriley@users.noreply.github.com>
2026-08-02 09:59:56 -0400

    Test standalone print document lifecycle

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { PrintDocument } from './print/PrintDocument'
 import './styles.css'
 
+const isPrintDocument = new URLSearchParams(window.location.search).get('print') === '1'
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><App /></StrictMode>,
+  <StrictMode>{isPrintDocument ? <PrintDocument /> : <App />}</StrictMode>,
 )
